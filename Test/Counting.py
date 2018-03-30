db662567139857c56fb552c1302dffad23f0a6c1
Wykett <bruno.chami@gmail.com>
2018-03-30 11:23:04 +0800

    First commit for test

---
+++ Test/Counting.py
@@ -0,0 +1,14 @@
+class Counting:
+	urls = [
+"http://www.google.com/a.txt",
+"http://www.google.com.tw/a.txt",
+"http://www.google.com/download/c.jpg",
+"http://www.google.co.jp/a.txt",
+"http://www.google.com/b.txt",
+"https://facebook.com/movie/b.txt",
+"http://yahoo.com/123/000/c.jpg",
+"http://gliacloud.com/haha.png",
+]
+
+	def count(self):
+		return 0
