5dbdb41c26afae58d4d462c240afe72e309f66ec
Wykett <bruno.chami@gmail.com>
2020-11-09 08:58:13 +0800

    Update BackspacesInStringKata.java

--- src/main/java/com/wykett/katas/BackspacesInStringKata.java
+++ src/main/java/com/wykett/katas/BackspacesInStringKata.java
@@ -4,27 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BackspacesInStringKata {
-  public String cleanString(String s) {
-    
-    List<Character> resultAsList = new ArrayList<Character>();
-    int charAdded = 0;
-    
-    for (int i = 0; i < s.length(); i++) {
-      
-      if (s.charAt(i) != '#') {
-        resultAsList.add(s.charAt(i));
-        charAdded++;
-      } else {
-        if (!resultAsList.isEmpty()) {
-          resultAsList.remove(charAdded - 1);
-          charAdded--;  
-        }
-      }
-    }
-    String result = "";
-    for (int i = 0; i < charAdded; i++) {
-      result += resultAsList.get(i);
-    }
-    return result;
-  }
+	public String cleanString(String s) {
+
+		StringBuilder resultStrBuilder = new StringBuilder();
+		List<Character> resultAsList = new ArrayList<Character>();
+		int charAdded = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+
+			if (s.charAt(i) != '#') {
+				resultStrBuilder.append(s.charAt(i));
+				resultAsList.add(s.charAt(i));
+				charAdded++;
+			} else {
+				if (!resultAsList.isEmpty()) {
+					resultAsList.remove(charAdded - 1);
+					charAdded--;
+				}
+			}
+		}
+		String result = "";
+		for (int i = 0; i < charAdded; i++) {
+			result += resultAsList.get(i);
+		}
+		return result;
+	}
 }
